a9d10c84eb7b32aa5859c6f8fa931f79a74052c9
Steven Perfors <stevecanbuild@gmail.com>
2020-01-28 21:20:43 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person 
+  def name=(new_name)
+    @name = new_name
+  end
+  
+  def name
+    @name 
+  end
+  
+  def job=(job)
+    @job = new_breed
+  end
+  
+  def breed
+    @breed 
+  end
+  
+end
