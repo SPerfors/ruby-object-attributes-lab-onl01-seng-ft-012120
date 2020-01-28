1ee4e456757c0b0052192a7c58f4fa45eae18022
Steven Perfors <stevecanbuild@gmail.com>
2020-01-28 21:18:12 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,13 +1,10 @@
 class Dog 
-  def initialize(name)
-    @name = name
+  def name=(new_name)
+    @dog_name = new_name
   end
   
   def name
-    @name 
+    @dog_name 
   end
   
-  def name=(new_name)
-    @name = new_name
-  end
 end
